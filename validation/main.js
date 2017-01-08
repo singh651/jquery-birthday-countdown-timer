edcae4e173375f51bc537503e0ab97d7d6ab79f1
dmytro-revak <dmytro27@gmail.com>
2017-01-08 21:24:31 +0200

    created function which get all user's values for future validation

--- validation/main.js
+++ validation/main.js
@@ -1,38 +1,42 @@
-
-(function initValidation() {
-  debugger
-  var $formsForValidate =  getElementForValidation();
-   $formsForValidate.each(function (index, element) {
-    $(element).on('submit' , function (e) {
+(function implementValidation() {
+  // Get all form to validate
+  var $formsForValidate = getElementForValidation();
+  // Start validation after click on "Send" button
+  $formsForValidate.each(function (index, element) {
+    $(element).on('submit', function (e, index) {
       e.preventDefault();
-      validation();
-    })
-   });
-
-
-   function validation() {
-     alert('validation');
-   }
-
-  var valuesForValidate = [];
-  $elementsForValidate.each(function (index, element) {
-    var celement = element.value;
-    // valuesForValidate.push(currentValue);
-  console.log(celement);
+      initValidation(index);
+    });
   });
 
 
 
+  //----------------------------------------------Start functions description----------------------------------------------------
+
+  // The following function implements all validation process
+  function initValidation(index) {
+    // Get user's values
+    var valuesForValidate = [];
+    valuesForValidate = getValidateValues(index);
+    // console.log(valuesForValidate);
+
+    // The following function get user's values for future validation
+    function getValidateValues(index) {
+      var elementsForValidate = $('input[type!="submit"]', $('[data-validate]').get(index));
+      elementsForValidate.each(function (index, element) {
+        valuesForValidate.push( $(element).val() );
+      });
+      return valuesForValidate;
+    }
+   }
+
+  // The following function selectes all form for validation
+  function getElementForValidation() {
+    $elementsForValidate = $('[data-validate]');
+    return $elementsForValidate;
+  }
 
 
 
-
-  //----------------------------------------------
-
-function getElementForValidation() {
-  $elementsForValidate = $('[data-validate]');
-return $elementsForValidate;
-}
-
-  //------------------------------
+  //---------------------------------------------End functions description-------------------------------------------------------
 })();

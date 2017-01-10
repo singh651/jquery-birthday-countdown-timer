b759e5a023798a7a7edf03f85ee97be72cde3c30
dmytro-revak <dmytro27@gmail.com>
2017-01-10 02:28:10 +0200

    fix field validation and almost finished with required and text validation

--- validation/main.js
+++ validation/main.js
@@ -18,46 +18,55 @@
 
     // Set validation rule for each filed
     var validationRules = {
-    'required' : function (value) {
+    'data-validate-required' : function (value, element) {
       if (!value) {
         alert('empty field');
       }
     },
     'name' : function (value, element) {
       debugger
-      var onlyLetters = /[a-zA-Z]/g;
-      var minLengt = $(element).attr('data-validate-minlength');
-      var maxLengt = $(element).attr('data-validate-maxlength');
+      var onlyLetters = /[a-zA-Z]/g,
+          minLengt = parseInt( $(element).attr('data-validate-minlength') ),
+          maxLengt = parseInt( $(element).attr('data-validate-maxlength') );
+
       if ( onlyLetters.test(value) ) {
         if (value.length > minLengt && value.length < maxLengt) {
 
         } else {
-        alert('min max length');
+          alert('min max length');
         }
       } else {
         alert('only letters');
       } 
-    }
+    },
 
    }
    
+
     // Get all input elements to validate from current form
     var $inputesForValidate = $( 'input[type!="submit"]', $('[data-validate]').get(index) );
     // Start fields validation
     fieldValidation($inputesForValidate);
 
-
     function fieldValidation($inputesForValidate) {
       $inputesForValidate.each(function (index, element) {
         debugger
-        var atributes = $(element).attributes;
-        if ( $(element).is('[data-validate-required]') ) {
-          validationRules.required( $(element).val() );
-        }
+        var currentInputValue = $(element).val();
+        var attributes = this.attributes;
+        $(attributes).each(function (index, attribute) {
+          debugger
+          var attributeName = attribute.name;
+          if ( validationRules.hasOwnProperty(attributeName) ) {
+          validationRules[attributeName](currentInputValue, element);
+          // validationRules.attribute( $(element).val() );
+          }
+        });
+        // if ( $(element).is('[data-validate-required]') ) {
+        // }
 
-        if ( $(element).is('[name = "name"]') ) {
-          validationRules.name( $(element).val() ); 
-        }
+        // if ( $(element).is('[name = "name"]') ) {
+        //   validationRules.name( $(element).val() ); 
+        // }
       });
 
     }
@@ -77,7 +86,7 @@
   function getElementForValidation() {
     $elementsForValidate = $('[data-validate]');
     return $elementsForValidate;
-  }
+  };
 
 
 

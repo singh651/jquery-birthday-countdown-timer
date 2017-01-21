47536e0e7abc1e9e089f48e82ac871579b78bf54
dmytro-revak <dmytro27@gmail.com>
2017-01-21 21:52:03 +0200

    finished with countdown function and created index page

--- countdown-timer/main.js
+++ countdown-timer/main.js
@@ -1,52 +1,59 @@
+(function initCountDownTimer() {
+      // debugger
 
+  // Following function implement count down process 
+  function countDown(countDowmTime) {
+    var timeIntervalId = setInterval(function () {
+      // Following object saves time settings for timer
+      var timesSettings = {
+        seconds: 3,
+        minutes: 2,
+        hours: 1,
+      };
+    // Following array saves names of countDown array elements
+    var position = ['seconds', 'minutes', 'hours', 'days'];
 
-function countDowm(countDowmTime) {
-  setInterval(function () {
-
-  var timesSettings = {
-    seconds: 3,
-    minutes: 4,
-    hours: 2
-  }
-
-  var position = ['seconds', 'minutes', 'hours', 'days'];
-
-  for (var i = 0; i < countDowmTime.length; i++) {
-    debugger
-    var currentElement = countDowmTime[i];
-    var nextElement = countDowmTime[i+1];
-    if (currentElement === 0) {
-      var a = verifyTheArrayElements(countDowmTime);
-      for (var a = 0; a < countDowmTime.length; a++) {
-        if (countDowmTime[a] === 0) {
-          countDowmTime[a] = timesSettings[position[a]];  
-        } else {
-          countDowmTime[a]--;
-          break;
-        }
-      }  
-      console.log(countDowmTime);
-      break;
-    } else {
-      countDowmTime[i] = --currentElement;
-      console.log(countDowmTime);
-      break;
+    // Core of count down process
+    for (var i = 0; i < countDowmTime.length; i++) {
+      var currentElement = countDowmTime[i];
+      // Check values of array elements
+      if ( verifyTheArrayElements(countDowmTime) ) {
+        clearInterval(timeIntervalId);
+        alert('done');
+        break;
+      } else if (currentElement === 0) {
+        for (var a = 0; a < countDowmTime.length; a++) {
+          if (countDowmTime[a] === 0) {
+            countDowmTime[a] = timesSettings[position[a]];  
+          } else {
+            countDowmTime[a]--;
+            break;
+          }
+        }  
+        console.log(countDowmTime);
+        break;
+      } else {
+        countDowmTime[i] = --currentElement;
+        console.log(countDowmTime);
+        break;
+      } 
     } 
-  } 
 
-
-
-
-  function verifyTheArrayElements(array) {
-    var isAllArrayElementsAreZero;
-    $(array).each(function (index, element) {
-      if ( $(element) !== 0 ) {
-        isAllArrayElementsAreZero = false;
-      }
-    }); 
-    return isAllArrayElementsAreZero;
-  }
-
-
+    // Following function verifies are each of array's elements zero 
+    function verifyTheArrayElements(array) {
+      var isAllArrayElementsAreZero = true;
+      $(array).each(function (index, element) {
+        if (element !== 0) {
+          isAllArrayElementsAreZero = false;
+        }
+      }); 
+      return isAllArrayElementsAreZero;
+    }
   }, 1000);
 }
+
+
+
+
+})();
+
